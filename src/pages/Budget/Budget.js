805c4ab720a817f34a716342c95c76af30a2a740
Jake Meyers <jakejake92@yahoo.com>
2021-02-03 19:00:05 -0700

    added stying to budget

--- src/pages/Budget/Budget.js
+++ src/pages/Budget/Budget.js
@@ -1,13 +1,36 @@
+import { Typography } from "@material-ui/core";
 import React, { useContext } from "react";
 import { ExpenseContext } from "../../State/ExpenseContext";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	App: {
+		expense: {
+			display: "flex",
+			flexDirection: "row",
+			marginLeft: 100,
+		},
+		name: {
+			display: "inline-block",
+		},
+	},
+}));
 
 const Budget = () => {
+	const classes = useStyles();
 	const expenses = useContext(ExpenseContext);
-	const title = expenses.map((expense) => {
-		return <h1>{expense.name}</h1>;
+	const expense = expenses.map((expense) => {
+		return (
+			<div className={classes.expense}>
+				<Typography variant='subtitle2' className={classes.name}>
+					{expense.Catagory}:
+				</Typography>
+				<Typography variant='subtitle1'>{expense.name}</Typography>
+			</div>
+		);
 	});
 
-	return <div>{title}</div>;
+	return <div>{expense}</div>;
 };
 
 export default Budget;
